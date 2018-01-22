1eb4144fda60a273c3037d98eb9c304c2ccf861b
ameliejyc <amelie.chan@gmail.com>
2018-01-22 22:26:16 +0000

    Add tax band table :money_with_wings:

--- src/components/infoPage/index.js
+++ src/components/infoPage/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Container } from './index.styles';
+import { Container, Table, Heading } from './index.styles';
 
 const InfoPage = () => {
   return (
     <Container>
       <h1>Info</h1>
+      <Table>
+        <tr>
+          <Heading>Band</Heading>
+          <Heading>Taxable Income</Heading>
+          <Heading>Tax Rate</Heading>
+        </tr>
+        <tr>
+          <td>Personal Allowance</td>
+          <td>Up to £11,500 🤘🏽</td>
+          <td>0%</td>
+        </tr>
+        <tr>
+          <td>Basic Rate</td>
+          <td>£11,501 to £45,000 💁🏻</td>
+          <td>20%</td>
+        </tr>
+        <tr>
+          <td>Higher Rate</td>
+          <td>£45,001 to £150,000 💅🏾</td>
+          <td>40%</td>
+        </tr>
+        <tr>
+          <td>Additional Rate</td>
+          <td>over £150,000 👀</td>
+          <td>45%</td>
+        </tr>
+      </Table>
     </Container>
   );
 };
--- src/components/infoPage/index.styles.js
+++ src/components/infoPage/index.styles.js
@@ -2,6 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   min-height: 100vh;
+`;
+
+export const Table = styled.table`
+  font-size: 2rem;
+  text-align: center;
+`;
+
+export const Heading = styled.th`
+  padding: 2rem 5rem;
 `;
